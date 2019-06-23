34783c2ef7141ac5aac8946d6845888be66e1649
Filip Lindahl <gysztaf@gmail.com>
2019-06-23 18:34:30 +0200

    css fixes for smaller screens and spaces.

--- src/Hour.tsx
+++ src/Hour.tsx
@@ -1,22 +1,22 @@
-import React from "react";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import { withStyles, WithStyles, Theme } from "@material-ui/core/styles";
-import { EventEntity } from "./eventInterface";
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { withStyles, WithStyles, Theme } from '@material-ui/core/styles';
+import { EventEntity } from './eventInterface';
 
 const styles: any = (theme: Theme) => ({
   hour: {
     height: (props: hourProps) => `calc((100% / (24/${props.height})))`,
-    backgroundColor: "#456",
-    border: "1px inset #456",
-    color: "#ddd"
+    backgroundColor: '#456',
+    border: '1px inset #456',
+    color: '#ddd'
   },
   hidden: {
-    visibility: "hidden"
+    visibility: 'hidden'
   },
   timeText: {
-    [theme.breakpoints.only("xs")]: {
-      display: "none"
+    [theme.breakpoints.only('xs')]: {
+      fontSize: '0.5rem'
     }
   }
 });
@@ -39,21 +39,32 @@ class Hour extends React.Component<hourProps, hourState> {
 
   public render() {
     const { classes } = this.props;
+    const time = this.props.event
+      ? `${this.props.event.startHour}:00 - ${this.props.event.endHour}:00`
+      : '';
+    const inlineTime =
+      this.props.event && this.props.event.duration < 2 ? 'inline' : 'initial';
     return (
       <Grid
         item
         xs={12}
         className={`${classes.hour} ${
-          this.props.hidden === true ? classes.hidden : ""
+          this.props.hidden === true ? classes.hidden : ''
         }`}
       >
-        <Typography variant="body1">
-          {this.props.event ? this.props.event.activity : ""}{" "}
+        <Typography variant="body1" display={inlineTime}>
+          {this.props.event ? `${this.props.event.activity}` : ''}
         </Typography>
-        <Typography variant="subtitle2">
+        <Typography
+          variant="subtitle2"
+          display={inlineTime}
+          className={inlineTime === 'inline' ? classes.timeText : ''}
+        >
           {this.props.event
-            ? `${this.props.event.startHour}:00 - ${this.props.event.endHour}:00`
-            : ""}
+            ? inlineTime === 'inline'
+              ? ` - ${time}`
+              : time
+            : ''}
         </Typography>
       </Grid>
     );
